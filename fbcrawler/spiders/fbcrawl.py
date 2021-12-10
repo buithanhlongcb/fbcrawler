c3df2aaed7087932fb58a1759875dc86ba55c523
Long Bui <buithanhlong.cb@.com>
2021-12-10 11:13:08 +0700

    Complete Login Crawl

--- fbcrawler/spiders/fbcrawl.py
+++ fbcrawler/spiders/fbcrawl.py
@@ -17,6 +17,7 @@ class FbBaseSpider(Spider):
         self.email = "longlong.031.2000@gmail.com"
         self.password = "LongLong031"
         self.flags= 0
+        self.flags_rep = 0
 
 
     def parse(self, response):
@@ -37,7 +38,7 @@ class FbBaseSpider(Spider):
         with open(filename, 'wb') as f:
             f.write(response.body)
 
-        id_posts = response.xpath('//div[@data-ft]/div[2]/div[2]/a/@href').extract()
+        id_posts = response.xpath('//div[@id="timelineBody"]/div[2]/div/div/div/div/div/div[2]/a[3]/@href').extract() 
         for id_post in id_posts:
                 href = 'http://mbasic.facebook.com' + id_post
                 yield response.follow(href, self.parse_comment)
@@ -53,9 +54,16 @@ class FbBaseSpider(Spider):
         #users = response.xpath('').extract()
         #users = [remove_accent(user) for user in users]
         if response.xpath('//div[@id="MPhotoContent"]') == []:
-            comments = response.xpath('//div[@id="root"]/div[2]/div[2]/div/div[2]/div/div')
+            comments = response.xpath('//div[@id="root"]/div/div[2]/div/div[5]/div/div') 
         else:
-            comments = response.xpath('//div[@id="MPhotoContent"]/div[2]/div/div/div[3]/div/div')
+            comments = response.xpath('//div[@id="MPhotoContent"]/div[2]/div/div/div[4]/div/div')
+        
+        if comments.xpath('./div[4]/div/div/a/@href').extract() != []:
+            rep_urls = comments.xpath('./div[4]/div/div/a/@href').extract()
+            for rep_url in rep_urls:
+                next_url = 'http://mbasic.facebook.com' + rep_url
+                yield response.follow(next_url, self.parse_comment_rep)
+
         for comment in comments:
             user = comment.xpath('./h3/a/text()').extract()
             tags = comment.xpath('./div[1]/a/text()').extract()
@@ -66,14 +74,20 @@ class FbBaseSpider(Spider):
                 'User': user,
                 'Comment': content,
                 'Tag': tags,
+                'html': self.flags-1
             }
         #comments = [re.compile(r'<[^>]+>').sub('', comment) for comment in comments]
         #comments = [remove_accent(comment) for comment in comments]
-        next_id_comment = response.xpath('//div[@id="root"]/div[2]/div[2]/div/div[2]/div/a/@href').extract()
-        next_url =''
+        next_id_comment = response.xpath('//div[@id="root"]/div/div[2]/div/div[5]/div/a/@href').extract()  
+
         if next_id_comment != []:
             next_url = 'http://mbasic.facebook.com' + next_id_comment[-1]
-            yield response.follow(next_url, self.parse_comment)
+            if len(next_id_comment) == 1:
+                if 'View previous comments' in response.xpath('//div[@id="root"]/div/div[2]/div/div[5]/div/a/text()')[0].extract():
+                    next_url =''
+
+            if next_url != '':
+                yield response.follow(next_url, self.parse_comment)
 
         #yield {
         #        'Url': url,
@@ -83,3 +97,27 @@ class FbBaseSpider(Spider):
         #        'Next_url': next_url
         #    }
         
+    def parse_comment_rep(self, response):
+        filename = 'rep_cmt' + str(self.flags_rep) +'.html'
+        self.flags_rep +=1
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        url = response.url
+
+        id_page = url[url.find('Acontent_owner_id_new.') + len('Acontent_owner_id_new.'):url.find('%', url.find('Acontent_owner_id_new.'))]
+        id_post = url[url.find('mf_story_key')+len('mf_story_key.'):url.find('%')]
+
+        comments = response.xpath('//div[@id="root"]/div/div[3]/div/div') 
+
+        for comment in comments:
+            user = comment.xpath('./h3/a/text()').extract()
+            tags = comment.xpath('./div[1]/a/text()').extract()
+            content = comment.xpath('./div[1]/text()').extract()
+            yield {
+                #'Url': url,
+                'Idpage - Idpost':id_page + " - "+id_post, 
+                'User': user,
+                'Comment': content,
+                'Tag': tags,
+                'html': str(self.flags_rep-1) + "rep"
+            }       
